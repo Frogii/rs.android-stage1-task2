38598e5d48c40087f4a94e47ec36d0bb35eb2358
Vlad Hilko <vlad.hilko@gmail.com>
2020-04-13 01:34:05 +0300

    task3: ok

--- src/main/kotlin/subtask3/Abbreviation.kt
+++ src/main/kotlin/subtask3/Abbreviation.kt
@@ -4,6 +4,15 @@ class Abbreviation {
 
     // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        val arrA = a.toUpperCase().toMutableList()
+        val arrB = b.toMutableList()
+        val output: MutableList<Char> = mutableListOf()
+
+        for (char in arrA) {
+            if (arrB.contains(char)) output.add(char)
+
+        }
+        return if (output == arrB) "YES"
+        else "NO"
     }
 }
