6ab584ce49509f8878f46f5041a45750172feb25
Vlad Hilko <vlad.hilko@gmail.com>
2020-04-14 00:16:26 +0300

    task5: ok task6: ok

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,40 @@
 package subtask5
 
+import java.lang.StringBuilder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
     // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        when (blockB) {
+            String::class -> {
+                val output = blockA.filterIsInstance<String>()
+                val builder = StringBuilder()
+                for (i in output) builder.append(i)
+                return builder.toString()
+            }
+            Int::class -> {
+                val output = blockA.filterIsInstance<Int>()
+                return output.sum()
+            }
+            else -> {
+                val output = blockA.filterIsInstance<LocalDate>()
+                return if (output.size == 1) {
+                    output[0].toString()
+                } else {
+                    val arr = mutableListOf<Long>()
+                    for (i in output) {
+                        if(i.toEpochDay() <= LocalDate.now().toEpochDay())
+                        arr.add(i.toEpochDay()-365)
+                    }
+                    arr.sort()
+                    val formatter = DateTimeFormatter.ofPattern("dd.MM.YYYY")
+                    return LocalDate.ofEpochDay(arr.last()).format(formatter)
+                }
+            }
+        }
     }
 }
--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -4,6 +4,20 @@ class Fibonacci {
 
     // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        var first = 0
+        var second = 1
+        var proizved = first * second
+
+        while (proizved < n) {
+            val count = first
+            first = second
+            second += count
+            proizved = first * second
+
+        }
+        return intArrayOf(first, second, if (proizved>n)0 else 1)
+
+
     }
 }
